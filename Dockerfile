6cab41c18b7cb9bd0cc9e359bed27791ed26ce4a
yozuru <d1322325603@gmail.com>
2025-12-28 04:37:10 +0800

    fix: 修改了dockerfile、支持语言列表

--- Dockerfile
+++ Dockerfile
@@ -9,17 +9,23 @@ LABEL source_code=$JUDGE0_SOURCE_CODE
 ENV JUDGE0_MAINTAINER "Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>"
 LABEL maintainer=$JUDGE0_MAINTAINER
 
-ENV PATH "/usr/local/ruby-2.7.0/bin:/opt/.gem/bin:$PATH"
-ENV GEM_HOME "/opt/.gem/"
+ENV GEM_HOME="/usr/local/bundle"
+ENV PATH $GEM_HOME/bin:$GEM_HOME/gems/bin:$PATH
 
 RUN apt-get update && \
     apt-get install -y --no-install-recommends \
       cron \
       libpq-dev \
-      sudo && \
+      sudo \
+      ruby-full \
+      ruby-dev \
+      nodejs \
+      npm \
+      build-essential \
+      && \
     rm -rf /var/lib/apt/lists/* && \
     echo "gem: --no-document" > /root/.gemrc && \
-    gem install bundler:2.1.4 && \
+    gem install bundler && \
     npm install -g --unsafe-perm aglio@2.3.0
 
 EXPOSE 2358
@@ -27,7 +33,8 @@ EXPOSE 2358
 WORKDIR /api
 
 COPY Gemfile* ./
-RUN RAILS_ENV=production bundle
+
+RUN RAILS_ENV=production bundle install
 
 COPY cron /etc/cron.d
 RUN cat /etc/cron.d/* | crontab -
@@ -39,13 +46,12 @@ CMD ["/api/scripts/server"]
 
 RUN useradd -u 1000 -m -r judge0 && \
     echo "judge0 ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers && \
-    chown judge0: /api/tmp/
+    chown -R judge0: /api/tmp/
 
 USER judge0
 
 ENV JUDGE0_VERSION "1.13.2"
 LABEL version=$JUDGE0_VERSION
-
 
 FROM production AS development
 
--- db/languages/active.rb
+++ db/languages/active.rb
@@ -15,11 +15,16 @@
     source_file: "a.out",
     run_cmd: "/bin/chmod +x a.out && ./a.out"
   },
+    {
+    id: 89,
+    name: "Multi-file program",
+    is_archived: true,
+  }
   # -------------------------------------------------------------------------
   # Bash (5.3)
   # -------------------------------------------------------------------------
   {
-    id: 46,
+    id:90,
     name: "Bash (5.3)",
     is_archived: false,
     source_file: "script.sh",
@@ -29,7 +34,7 @@
   # C Language (GCC 15.2.0)
   # -------------------------------------------------------------------------
   {
-    id: 48,
+    id: 91,
     name: "C (GCC 15.2.0)",
     is_archived: false,
     source_file: "main.c",
@@ -37,7 +42,7 @@
     run_cmd: "./a.out"
   },
   {
-    id: 49,
+    id: 92,
     name: "C (GCC 15.2.0 C99)",
     is_archived: false,
     source_file: "main.c",
@@ -48,7 +53,7 @@
   # C++ Language (GCC 15.2.0)
   # -------------------------------------------------------------------------
   {
-    id: 52,
+    id: 93,
     name: "C++ (GCC 15.2.0)",
     is_archived: false,
     source_file: "main.cpp",
@@ -56,7 +61,7 @@
     run_cmd: "./a.out"
   },
   {
-    id: 53,
+    id: 94,
     name: "C++ 17 (GCC 15.2.0)",
     is_archived: false,
     source_file: "main.cpp",
@@ -64,7 +69,7 @@
     run_cmd: "./a.out"
   },
   {
-    id: 54,
+    id: 95,
     name: "C++ 20 (GCC 15.2.0)",
     is_archived: false,
     source_file: "main.cpp",
@@ -75,7 +80,7 @@
   # Java (OpenJDK 25 & 21)
   # -------------------------------------------------------------------------
   {
-    id: 62,
+    id: 96,
     name: "Java (OpenJDK 25)",
     is_archived: false,
     source_file: "Main.java",
@@ -83,7 +88,7 @@
     run_cmd: "/usr/local/openjdk25/bin/java Main"
   },
   {
-    id: 90, 
+    id: 97, 
     name: "Java (OpenJDK 21)",
     is_archived: false,
     source_file: "Main.java",
@@ -94,7 +99,7 @@
   # Python (3.14.2)
   # -------------------------------------------------------------------------
   {
-    id: 71,
+    id: 98,
     name: "Python (3.14.2)",
     is_archived: false,
     source_file: "script.py",
@@ -104,7 +109,7 @@
   # Kotlin (2.3.0 & 1.9.0)
   # -------------------------------------------------------------------------
   {
-    id: 78,
+    id: 99,
     name: "Kotlin (2.3.0)",
     is_archived: false,
     source_file: "Main.kt",
@@ -112,11 +117,12 @@
     run_cmd: "/usr/local/kotlin-2.3.0/bin/kotlin MainKt"
   },
   {
-    id: 91,
+    id: 100,
     name: "Kotlin (1.9.0)",
     is_archived: false,
     source_file: "Main.kt",
     compile_cmd: "/usr/local/kotlin-1.9.0/bin/kotlinc %s Main.kt",
     run_cmd: "/usr/local/kotlin-1.9.0/bin/kotlin MainKt"
-  }
+  },
+
 ]
